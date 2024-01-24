429dd7b7f7d4a59f7b973bef22f5b6e9f5d5a51e
lucasdav <lucassouzadavanso@gmail.com>
2024-01-24 00:17:39 -0300

    inclusao main component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import "../styles/components/sidebar.sass";
 const Sidebar = () => {
     return (
         <aside id="sidebar">
-            <p>imagem</p>
+            <img src={Avatar} alt="Lucas Souza Davanso" />
             <p className="title">Software Developer</p>
             <p>redes sociais</p>
             <p>informações de contato</p>
