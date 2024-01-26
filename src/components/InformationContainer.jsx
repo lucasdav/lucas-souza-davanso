686b0a55164fc30cfcdb5197d32a6edb9aec5899
lucasdav <lucassouzadavanso@gmail.com>
2024-01-25 23:48:34 -0300

    main components

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -8,13 +8,24 @@ const InformationContainer = () => {
         <section id="information">
             <div className="info-card">
                 <AiFillPhone id="phone-icon" />
-                <h3>Telefone</h3>
-                <p>(17) 99632-5685</p>
+                <div>                    
+                    <h3>Telefone</h3>
+                    <p>(17) 99632-5685</p>
+                </div>
             </div>
             <div className="info-card">
                 <AiOutlineMail id="email-icon" />
-                <h3>E-mail</h3>
-                <p>lucassouzadavanso@gmail.com</p>
+                <div>
+                    <h3>E-mail</h3>
+                    <p>lucassouzadavanso@gmail.com</p>
+                </div>
+            </div>
+            <div className="info-card">
+                <AiFillEnvironment id="pin-icon" />
+                <div>
+                    <h3>Localização</h3>
+                    <p>Santa Fé do Sul - SP</p>
+                </div>
             </div>
         </section>
     )
