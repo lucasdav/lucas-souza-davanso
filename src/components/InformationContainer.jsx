15f8e85ed554f9e47d6681c21b75b73f42a2c95f
lucasdav <lucassouzadavanso@gmail.com>
2024-01-25 23:09:32 -0300

    add contato

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -8,6 +8,13 @@ const InformationContainer = () => {
         <section id="information">
             <div className="info-card">
                 <AiFillPhone id="phone-icon" />
+                <h3>Telefone</h3>
+                <p>(17) 99632-5685</p>
+            </div>
+            <div className="info-card">
+                <AiOutlineMail id="email-icon" />
+                <h3>E-mail</h3>
+                <p>lucassouzadavanso@gmail.com</p>
             </div>
         </section>
     )
