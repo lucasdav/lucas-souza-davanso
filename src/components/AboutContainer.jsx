b603a0221895fe72c66322cf6267199dd7fb51f0
lucasdav <lucassouzadavanso@gmail.com>
2024-01-26 22:28:22 -0300

    estilizacao main content

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,7 +1,19 @@
 
 const AboutContainer = () => {
     return (
-        <div>AboutContainer</div>
+        <section className="about-container">
+            <h2>Sobre</h2>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus placeat, eius omnis numquam ipsum 
+                laboriosam nisi iure molestias nam ullam iste, blanditiis ex repellat accusantium minus corporis dignissimos 
+                aut similique.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus placeat, eius omnis numquam ipsum 
+                laboriosam nisi iure molestias nam ullam iste, blanditiis ex repellat accusantium minus corporis dignissimos 
+                aut similique.
+            </p>
+        </section>
     )
 }
 
