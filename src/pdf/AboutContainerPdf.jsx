971f2968f557234713aea0632e8fcca37e6bf870
lucasdav <lucassouzadavanso@gmail.com>
2024-01-30 23:06:25 -0300

    ajuste aboute pdf

--- src/pdf/AboutContainerPdf.jsx
+++ src/pdf/AboutContainerPdf.jsx
@@ -1,14 +1,16 @@
+import "./styles/aboutPdf.sass";
 
 const AboutContainerPdf = () => {
     return (
-        <section className="about-container">
-            <h2>Sobre</h2>
-            <p>
-                - Desenvolvimento em react de um sistema web para gerência de carteiras para o banco BNB <br/>
-                - Tech Lead na squad de Investimentos Ecommerce Pf no Itaú, 3x PRAD premiado por alta performance <br/>
-                - 12 anos de experiência atuando com tecnologia e desenvolvimento frontend e backend <br/>
-                - Em aprendizado contínuo, integrado a inovações e compartilhando conhecimentos <br/>
-            </p>
+        <section className="about-container-pdf">
+            <div>
+                <h2>Sobre</h2>
+                <p>- Desenvolvimento em react de um sistema web para gerência de carteiras para o banco BNB </p>
+                <p>- Tech Lead na squad de Investimentos Ecommerce Pf no Itaú, 3x PRAD premiado por alta performance </p>
+                <p>- 12 anos de experiência atuando com tecnologia e desenvolvimento frontend e backend </p>
+                <p>- Em aprendizado contínuo, integrado a inovações e compartilhando conhecimentos </p>
+                
+            </div>
         </section>
     )
 }
