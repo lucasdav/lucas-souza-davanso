007108d39684d8252e2fcc6a18a3408821d0e0b2
lucasdav <lucassouzadavanso@gmail.com>
2024-01-30 23:52:22 -0300

    ajuste experience pdf

--- src/pdf/ExperiencePdf.jsx
+++ src/pdf/ExperiencePdf.jsx
@@ -2,66 +2,65 @@ import './styles/experiencePdf.sass'
 
 const ExperiencePdf = () => {
     return (
-        <section className='experience-container'>
-            <h2>Experiências</h2>
-            <div className="experience-grid">
+        <section className='experience-container-pdf'>
+            <div className="experience-grid-pdf">
+                <h2>Experiências</h2>
                 
-                <div className="experience-card">                    
-                    <div className="experience-info">
+                <div className="experience-card-pdf">                    
+                    <div className="experience-info-pdf">
                         <h3>Software Developer na ENGESOFTWARE TECNOLOGIA S.A.</h3>
                         <h4>Out. de 2023 - Atual - 4 meses</h4>
-                        <p>
-                        Criação de um sistema de gerência de carteiras para o banco BNB, utilizando framework React.<br/>
-                        - Tabelas dinâmicas expansivas com ordenadores utilizando material-ui react-table<br/>
-                        - Impressão de relatórios para download pdf utilizando react-to-pdf<br/>
-                        - Componentes dinamicos utilizando material-ui<br/>
-                        - Gráficos dinâmicos utilizando material-ui react-charts<br/>
-                        - Domínio em testes automatizados (Unitários, Integração e e2e);<br/>
-                        - Experiência com Hooks useState, useEffect e useMemo<br/>
-                        - Desenvolvimento em typescript, html, javascript, css<br/>
-                        - Integração via APIs Rest<br/>
-                        - Versionamento de código com RTC<br/>
-                        - Documentação de projetos: azure e figma<br/>
-                        </p>
+                        <p>Criação de um sistema de gerência de carteiras para o banco BNB, utilizando framework React.</p>
+                        <p>- Tabelas dinâmicas expansivas com ordenadores utilizando material-ui react-table</p>
+                        <p>- Impressão de relatórios para download pdf utilizando react-to-pdf</p>
+                        <p>- Componentes dinamicos utilizando material-ui</p>
+                        <p>- Gráficos dinâmicos utilizando material-ui react-charts</p>
+                        <p>- Domínio em testes automatizados (Unitários, Integração e e2e);</p>
+                        <p>- Experiência com Hooks useState, useEffect e useMemo</p>
+                        <p>- Desenvolvimento em typescript, html, javascript, css</p>
+                        <p>- Integração via APIs Rest</p>
+                        <p>- Versionamento de código com RTC</p>
+                        <p>- Documentação de projetos: azure e figma</p>
+                        
                     </div>
                 </div>
-                <div className="experience-card">                    
-                    <div className="experience-info">
+                <div className="experience-card-pdf">                    
+                    <div className="experience-info-pdf">
                         <h3>Software Engineer no Itaú Unibanco</h3>
                         <h4>Jul. de 2017 - Out de 2023 - 6 anos e 4 meses</h4>
-                        <p>
-                        - Tech Lead na squad de Investimentos PF na comunidade de Shared Experience<br/>
-                        - Mentoria e acompanhamento de boas práticas da equipe técnica<br/> 
-                        - Criação de novas aplicações, fomentando plano de desenvolvimento técnico<br/>
-                        - Desenvolvimento de aplicações mobile e web, utilizando framework angular, nas 
+                        
+                        <p>- Tech Lead na squad de Investimentos PF na comunidade de Shared Experience</p>
+                        <p>- Mentoria e acompanhamento de boas práticas da equipe técnica</p> 
+                        <p>- Criação de novas aplicações, fomentando plano de desenvolvimento técnico</p>
+                        <p>- Desenvolvimento de aplicações mobile e web, utilizando framework angular, nas 
                         linguagens c#, .Net, HTML, javascript, css, java, kotlin e typescript, uso de reactive 
-                        state NGRX<br/>
-                        - Domínio de técnicas de otimização do tempo de carregamentos das páginas, 
-                        performance e conversão<br/>
-                        - Documentação de projetos com figma e confluence<br/>
-                        - Experiência em testes automatizados (Unitários, Integração e e2e)<br/>
-                        - Cultura devops e ferramentas: Sonar, Fortify, Docker e Kubernetes<br/>
-                        - Versionamento de código com Git<br/>
-                        - Acompanhamento de implantação, homologação e releases via Github Action e AWS Pipelines<br/>
-                        - Observabilidade: Splunk e App Dynamics<br/>
-                        - Experiência com metodologias de desenvolvimento ágil, como Scrum e Kanban<br/>
-                        - Garantir funcionamento e equalização do ambiente Docker<br/>
-                        - Referência técnica em front-end utilizando framework Angular<br/>
-                        - Principais Iniciativas: Criação de espaço de campanha h2o integrado com cms headless na 
+                        state NGRX</p>
+                        <p>- Domínio de técnicas de otimização do tempo de carregamentos das páginas, 
+                        performance e conversão</p>
+                        <p>- Documentação de projetos com figma e confluence</p>
+                        <p>- Experiência em testes automatizados (Unitários, Integração e e2e)</p>
+                        <p>- Cultura devops e ferramentas: Sonar, Fortify, Docker e Kubernetes</p>
+                        <p>- Versionamento de código com Git</p>
+                        <p>- Acompanhamento de implantação, homologação e releases via Github Action e AWS Pipelines</p>
+                        <p>- Observabilidade: Splunk e App Dynamics</p>
+                        <p>- Experiência com metodologias de desenvolvimento ágil, como Scrum e Kanban</p>
+                        <p>- Garantir funcionamento e equalização do ambiente Docker</p>
+                        <p>- Referência técnica em front-end utilizando framework Angular</p>
+                        <p>- Principais Iniciativas: Criação de espaço de campanha h2o integrado com cms headless na 
                         webview home de investimento para ofertas segmentadas. Desenvolvimento de micro 
                         front-end e integração com cms headless para modularizar elementos
                         </p>
                     </div>
                 </div>
-                <div className="experience-card">                    
-                    <div className="experience-info">
+                <div className="experience-card-pdf">                    
+                    <div className="experience-info-pdf">
                         <h3>Software Engineer na Indra</h3>
                         <h4>Jul. de 2016 - Jul. de 2017 - 1 ano e 1 mês</h4>
-                        <p>
-                        - Desenvolvimento em .Net do sistema web da Mapfre Seguros<br/>
-                        - Criação de manuais de usuário para o sistema<br/>
-                        - Suporte e manutenção no banco de dados do sistema SGD<br/>
-                        </p>
+                        
+                        <p>- Desenvolvimento em .Net do sistema web da Mapfre Seguros</p>
+                        <p>- Criação de manuais de usuário para o sistema</p>
+                        <p>- Suporte e manutenção no banco de dados do sistema SGD</p>
+                        
                     </div>
                 </div>
             </div>
--- src/pdf/MainContentPdf.jsx
+++ src/pdf/MainContentPdf.jsx
@@ -1,12 +1,14 @@
 import "./styles/maincontentPdf.sass"
 import SidebarPdf from "./SidebarPdf"
 import AboutContainerPdf from "./AboutContainerPdf"
+import ExperiencePdf from "./ExperiencePdf"
 
 const MainContentPdf = () => {
     return (
         <main id="main-content-pdf">
             <SidebarPdf/>
             <AboutContainerPdf/>
+            <ExperiencePdf/>
         </main>
     )
 }
