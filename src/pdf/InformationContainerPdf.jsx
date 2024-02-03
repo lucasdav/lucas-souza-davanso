0f8934ffa6bb510b0d27356f596f75ffe63c7741
lucasdav <lucassouzadavanso@gmail.com>
2024-02-02 23:50:24 -0300

    ajuste icones pdf

--- src/pdf/InformationContainerPdf.jsx
+++ src/pdf/InformationContainerPdf.jsx
@@ -7,25 +7,31 @@ const InformationContainerPdf = () => {
     return (
         <section id="information-pdf">
             <div className="info-card-pdf">
-                <AiFillPhone id="phone-icon" />
-                <div>                    
-                    <h3>Telefone</h3>
+                <div>
+                    <div className="flex-div">
+                        <AiFillPhone id="phone-icon" />
+                        <h3>Telefone</h3>
+                    </div>                    
                     <p>(17) 99632-5685</p>
                 </div>
             </div>
             <div className="lineStyled-pdf"></div>
             <div className="info-card-pdf">
-                <AiOutlineMail id="email-icon" />
                 <div>
-                    <h3>E-mail</h3>
+                    <div className="flex-div">
+                        <AiOutlineMail id="email-icon" />
+                        <h3>E-mail</h3>
+                    </div>
                     <p>lucassouzadavanso@gmail.com</p>
                 </div>
             </div>
             <div className="lineStyled-pdf"></div>
             <div className="info-card-pdf">
-                <AiFillEnvironment id="pin-icon" />
                 <div>
-                    <h3>Localização</h3>
+                    <div className="flex-div">
+                        <AiFillEnvironment id="pin-icon" />
+                        <h3>Localização</h3>
+                    </div>
                     <p>Santa Fé do Sul - SP</p>
                 </div>
             </div>
