92bc86dc98126fdd54ece997b36553f433d30083
lucasdav <lucassouzadavanso@gmail.com>
2024-04-04 22:12:52 -0300

    atualizacao formacao backend

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -5,14 +5,14 @@ const Certifications = () => {
         <section className='certification-container'>
             <h2>Certificados</h2>
             <div className="certification-grid">
-                
-                <div className="certification-card">                    
+
+                <div className="certification-card">
                     <div className="certification-info">
                         <h3>AWS Certified Cloud Practitioner</h3>
                         <h4>Amazon Web Services - expira em 2026</h4>
                         <div className='div-button'>
-                            <a 
-                                href="https://www.credly.com/badges/641042b6-c216-4713-af41-046b45b5e146/linked_in_profile" 
+                            <a
+                                href="https://www.credly.com/badges/641042b6-c216-4713-af41-046b45b5e146/linked_in_profile"
                                 className="btn"
                             >
                                 mostrar credencial
@@ -20,46 +20,60 @@ const Certifications = () => {
                         </div>
                     </div>
                 </div>
-                <div className="certification-card">                    
+                <div className="certification-card">
                     <div className="certification-info">
                         <h3>Formação React com Typescript</h3>
                         <h4>Março de 2024</h4>
                         <div className='div-button'>
-                            <a 
-                                href="https://cursos.alura.com.br/degree/certificate/54997aee-8207-4083-83d6-8aa4db454872" 
+                            <a
+                                href="https://cursos.alura.com.br/degree/certificate/54997aee-8207-4083-83d6-8aa4db454872"
                                 className="btn"
                             >
                                 mostrar credencial
                             </a>
-                        </div>                     
+                        </div>
                     </div>
                 </div>
-                <div className="certification-card">                    
+                <div className="certification-card">
                     <div className="certification-info">
                         <h3>Formação Angular</h3>
                         <h4>Dezembro de 2019</h4>
                         <div className='div-button'>
-                            <a 
-                                href="https://cursos.alura.com.br/degree/certificate/46473ba3-60ea-401e-ac1b-1100ace8fd1b" 
+                            <a
+                                href="https://cursos.alura.com.br/degree/certificate/46473ba3-60ea-401e-ac1b-1100ace8fd1b"
                                 className="btn"
                             >
                                 mostrar credencial
                             </a>
-                        </div>                     
+                        </div>
                     </div>
                 </div>
-                <div className="certification-card">                    
+                <div className="certification-card">
                     <div className="certification-info">
                         <h3>Formação Vue.js</h3>
                         <h4>Dezembro de 2019</h4>
                         <div className='div-button'>
-                            <a 
-                                href="https://cursos.alura.com.br/degree/certificate/6ec0054b-4e86-4201-b372-73dba41963b7" 
+                            <a
+                                href="https://cursos.alura.com.br/degree/certificate/6ec0054b-4e86-4201-b372-73dba41963b7"
                                 className="btn"
                             >
                                 mostrar credencial
                             </a>
-                        </div>                     
+                        </div>
+                    </div>
+                </div>
+                <div className="certification-card">
+                    <div className="certification-info">
+                        <h3>JavaScript com foco no back-end</h3>
+                        <h4>Abril de 2024</h4>
+                        <div className='div-button'>
+                            <a
+                                href="https://cursos.alura.com.br/user/lucasDavanso/degree-js-backend-v210587-210587/certificate"
+                                className="btn"
+                            >
+                                mostrar credencial
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
